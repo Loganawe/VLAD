8b68e4ab3b97306291db0530c83c2642bf6a9e57
Doowon <Doowon@10.13.65.97>
2020-02-07 01:59:51 -0700

    Asks for number of group members and saves it as an int

--- src/GroupMInput.java
+++ src/GroupMInput.java
@@ -1,4 +1,15 @@
+import java.util.Scanner; 
 
-public class GroupMInput {
-
+public class GroupMInput 
+{
+	int members = 0;
+	
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("How many group members?: ");
+		int members = keyboard.nextInt();
+		System.out.println(members);
+		
+	}
 }
