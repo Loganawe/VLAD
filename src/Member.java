7dae76182cbd6fc6e2f4013d7b1c1c4d44df6ff2
Doowon <Doowon@10.12.86.55>
2020-02-18 18:28:19 -0700

    Right now the only issues are that it asks really basic questions and that one member can have multiple roles but other then that it works fine. It uses the Member class to store the scores of each person by creating an object for each member. Theres also no names for each member right now it just calls the group member #

---
+++ src/Member.java
@@ -0,0 +1,35 @@
+
+public class Member 
+{
+
+	private int leaderscore;
+	private int organscore;
+	private int finscore;
+	private int impscore;
+	
+	
+	public int getImpscore() {
+		return impscore;
+	}
+	public void setImpscore(int impscore) {
+		this.impscore = impscore;
+	}
+	public int getLeaderscore() {
+		return leaderscore;
+	}
+	public void setLeaderscore(int leaderscore) {
+		this.leaderscore = leaderscore;
+	}
+	public int getOrganscore() {
+		return organscore;
+	}
+	public void setOrganscore(int organscore) {
+		this.organscore = organscore;
+	}
+	public int getFinscore() {
+		return finscore;
+	}
+	public void setFinscore(int finscore) {
+		this.finscore = finscore;
+	}
+}
