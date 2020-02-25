d04e27011e13256c5f40474aad8650a8bb879f26
Logan <Logan@169.254.208.149>
2020-02-24 17:45:32 -0700

    updated

---
+++ LoganBranch/VLAD/src/Group.java
@@ -0,0 +1,187 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Group {
+	private String groupName;
+	private ArrayList<Individual> members = new ArrayList<Individual>();
+	private ArrayList<int[]> groupAvailability = new ArrayList<int[]>();
+	
+	public Group(String gName, ArrayList<Individual> memberList, ArrayList<int[]> gAvailability) {
+		groupName = gName;
+		members = memberList;
+		groupAvailability = gAvailability;
+	}
+	
+	public Group(String gName, ArrayList<Individual> memberList) {
+		groupName = gName;
+		members = memberList;
+	}
+	
+	public Group(String gName) {
+		groupName = gName;
+	}
+	
+	public void setMembers(ArrayList<Individual> memberList) {
+		members = memberList;
+	}
+	
+	public void addMember(Individual member) {
+		members.add(member);
+	}
+	
+	public int getNumberOfMembers() {
+		return members.size();
+	}
+	
+	public String getName() {
+		return groupName;
+	}
+	
+	public ArrayList<int[]> getGroupUnavailability() {
+		return new ArrayList<int[]>(groupAvailability);
+	}
+	
+	public boolean checkDayExist(int[] time) {
+		ArrayList<Integer> whichDays = new ArrayList<Integer>();
+		for (int[] index: groupAvailability) {
+			if (time[0] == index[0]) {
+				whichDays.add(time[0]);
+			}
+		}
+		if (whichDays.size() >= 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void checkAvailability(ArrayList<int[]> times) {
+		int numbersOfTimes = times.size();
+		for (int i = 0; i < numbersOfTimes; i++) {
+			if (groupAvailability.contains(times.get(i))) {
+				// do nothing
+				//System.out.println("Same already exists");
+			}
+			else if (checkDayExist(times.get(i))) {
+				
+				int secondNumberOftimes = groupAvailability.size();
+				for (int index = 0; index < secondNumberOftimes; index++) {
+					
+					if (times.get(i)[0] == groupAvailability.get(index)[0]) {
+						if (times.get(i)[1] <= groupAvailability.get(index)[1] && times.get(i)[2] >= groupAvailability.get(index)[2] ) {
+							groupAvailability.get(index)[1] = times.get(i)[1];
+							groupAvailability.get(index)[2] = times.get(i)[2];
+							//System.out.println("bigger than old");
+						}
+						else if (times.get(i)[1] >= groupAvailability.get(index)[1] && times.get(i)[2] <= groupAvailability.get(index)[2] ) {
+							// do nothing
+							//System.out.println("smaller than old");
+						}
+						else if (times.get(i)[1] < groupAvailability.get(index)[1] && times.get(i)[2] >= groupAvailability.get(index)[1] ) {
+							groupAvailability.get(index)[1] = times.get(i)[1];
+							//System.out.println("starts before, but is during");
+						}
+						else if (times.get(i)[1] <= groupAvailability.get(index)[2] && times.get(i)[2] > groupAvailability.get(index)[2] ) {
+							groupAvailability.get(index)[2] = times.get(i)[2];
+							//System.out.println("ends after, but is during");
+						}
+						else if (times.get(i)[2] < groupAvailability.get(index)[1] || times.get(i)[1] > groupAvailability.get(index)[2] ) {
+							groupAvailability.add(times.get(i));
+							//System.out.println("Same day, but not same or overlapping times");
+						}
+					}
+
+				}
+				
+			}
+			else {
+				groupAvailability.add(times.get(i));
+				//System.out.println("Day doesn't exist");
+			}
+			
+		
+		
+		}
+	}
+	
+	public void setAvailability() {
+		groupAvailability.add(members.get(0).getUnavailability().get(0));
+		for (Individual person: members) {
+			checkAvailability(person.getUnavailability());
+		}
+	}
+	
+	public String toString() {
+		String availability = "";
+		String returnStatment = "";
+		for (int[] busyAt: groupAvailability) {
+			if (busyAt[0] == 1) {
+				availability += "Sunday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 2) {
+				availability += "Monday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 3) {
+				availability += "Tuesday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 4) {
+				availability += "Wednesday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 5) {
+				availability += "Thursday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 6) {
+				availability += "Friday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 7) {
+				availability += "Saturday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+		}
+		returnStatment = groupName + " is unavailable on " + availability + "please plan around this.";
+		return returnStatment;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Individual> people = new ArrayList<Individual>();
+		
+		Scanner ginput = new Scanner(System.in);
+		
+		System.out.print("Enter your group name: ");
+		String name = ginput.nextLine();
+		
+		
+		String quit = "fdhasdh";
+		while (!(quit.isEmpty())) {
+			System.out.println(" ");
+			System.out.print("Would you like to add a group member? (enter 'yes', or nothing to quit): ");
+			quit = ginput.nextLine();
+			
+			if (quit.equals("yes")) {
+				Individual newMember = new Individual();
+				people.add(newMember.setIndividual());
+			}
+			if (!(quit.equals("yes")) && !(quit.isEmpty())) {
+				System.out.println("Please enter 'yes', or leave the field blank to exit");
+				System.out.println(" ");
+			}
+		}
+		ginput.close();
+		
+		Group newGroup = new Group(name);
+		
+		if (!(people.isEmpty())) {
+			newGroup.setMembers(people);
+			newGroup.setAvailability();
+		}
+		
+		
+		if (newGroup.getGroupUnavailability().size() >= 1) {
+			System.out.println(" ");
+			System.out.println(newGroup.toString());
+		}
+	}
+
+
+
+}
---
+++ LoganBranch/VLAD/src/Individual.java
@@ -0,0 +1,120 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Individual {
+
+	private String memberName;
+	ArrayList<int[]> unavailableList = new ArrayList<int[]>();
+	
+	public Individual() {
+		//empty constructor
+	}
+	
+	public Individual(String name) {
+		memberName = name;
+	}
+	
+	public Individual(ArrayList<int[]> availability) {
+		unavailableList = availability;
+	}
+	
+	public Individual(String name, ArrayList<int[]> availability) {
+		memberName = name;
+		unavailableList = availability;
+	}
+	
+	public void setAddBadDate(int[] busyAt) {
+		unavailableList.add(busyAt);
+	}
+	
+	public void setName(String name) {
+		memberName = name;
+	}
+	
+	public void setAvailability(ArrayList<int[]> availability) {
+		unavailableList = availability;
+	}
+	
+	public String getName() {
+		return memberName;
+	}
+	
+	public ArrayList<int[]> getUnavailability() {
+		return new ArrayList<int[]>(unavailableList);
+	}
+	
+	public void setAvailability(int day, int startTime, int endTime) {
+		int[] busyAt = {day, startTime, endTime};
+		unavailableList.add(busyAt);
+	}
+
+	
+	public String toString() {
+		String availability = "";
+		String returnStatment = "";
+		for (int[] busyAt: unavailableList) {
+			if (busyAt[0] == 1) {
+				availability += "Sunday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 2) {
+				availability += "Monday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 3) {
+				availability += "Tuesday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 4) {
+				availability += "Wednesday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 5) {
+				availability += "Thursday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 6) {
+				availability += "Friday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+			if (busyAt[0] == 7) {
+				availability += "Saturday from " + busyAt[1] + " - " + busyAt[2] + ", ";
+			}
+		}
+		returnStatment = memberName + " is unavailable on " + availability + "please plan around this.";
+		return returnStatment;
+	}
+	
+	public Individual setIndividual()  {
+		ArrayList<int[]> availability = new ArrayList<int[]>();
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your name: ");
+		String name = input.nextLine();
+		
+		int quit = 0;
+		while (quit != 1) {
+			System.out.print("What day are you unavailable? (enter 10 to quit): ");
+			int day = input.nextInt();
+			if (day == 10) {
+				quit = 1;
+			}
+			else {
+				System.out.print("Starting at what time?: ");
+				int start = input.nextInt();
+				
+				System.out.print("Ending when?: ");
+				int end = input.nextInt();
+				
+				System.out.println(" ");
+				
+				int[] busyAt = {day, start, end};
+				availability.add(busyAt);
+			}
+			
+		}
+		
+		Individual person = new Individual(name, availability);
+		
+		
+		
+		return person;
+	}
+
+}
